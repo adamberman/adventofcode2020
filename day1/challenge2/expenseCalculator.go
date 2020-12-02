8b9d2bd51f2ebadf2e5f523d77dcddca4605727b
Adam Berman <adamberman13@gmail.com>
2020-12-02 13:41:21 -0800

    Advent of code day 1, challenge 2

---
+++ day1/challenge2/expenseCalculator.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var filePath string
+	if len(os.Args) > 1 {
+		filePath = os.Args[1]
+	} else {
+		log.Fatal("Please provide a file path as an argument")
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var arr []int
+	for scanner.Scan() {
+		val, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		arr = append(arr, val)
+	}
+	for i := range arr {
+		for j := range arr {
+			for k := range arr {
+				if i == j || i == k || j == k {
+					continue
+				}
+				if arr[i]+arr[j]+arr[k] == 2020 {
+					fmt.Println(arr[i] * arr[j] * arr[k])
+				}
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
